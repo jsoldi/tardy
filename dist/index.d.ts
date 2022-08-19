3c33b8666a3cc114a511f07c9b55cf5a97ebcb2c
Juan Soldi <jsoldi@hotmail.com>
2022-08-19 19:22:00 +0200

    declarations

---
+++ dist/index.d.ts
@@ -0,0 +1,2 @@
+export { Tardy } from "./tardy.js";
+export { ITardyClient, TardyClient, CliTardyClient } from './client.js';
---
+++ dist/tardy.d.ts
@@ -0,0 +1,20 @@
+import { ITardyClient, TardyClient } from "./client.js";
+declare type Awaitable<T> = T | Promise<T>;
+export declare class Tardy<out T> {
+    readonly run: (client: TardyClient) => Promise<T>;
+    constructor(run: (client: TardyClient) => Promise<T>);
+    static of<T>(value: T): Tardy<T>;
+    /**
+     * This corresponds to calling an async function to create a promise.
+     */
+    static lift<A extends any[], T>(f: (this: TardyClient, ...args: A) => Promise<T>): (...args: A) => Tardy<T>;
+    static get client(): Tardy<TardyClient>;
+    static all<T>(progresses: Tardy<T>[]): Tardy<T[]>;
+    static seq<T>(progresses: Tardy<T>[]): Tardy<T[]>;
+    bind<U>(f: (value: T) => Awaitable<Tardy<U>>): Tardy<U>;
+    log(msg: string): Tardy<T>;
+    map<U>(f: (value: T) => U): Tardy<U>;
+    report(title?: string | null, min?: number, max?: number): Tardy<T>;
+    exec(mode?: ITardyClient | 'cli' | 'silent' | 'plain'): Promise<T>;
+}
+export {};
